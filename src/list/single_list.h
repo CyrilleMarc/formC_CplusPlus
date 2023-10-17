9731258a27ea544d8cbcb3a7598cc4ecc06687ff
CyrilleMarc <cyrillemarc1@gmail.com>
2023-10-17 22:50:28 +0200

    single_list

--- src/list/single_list.h
+++ src/list/single_list.h
@@ -1,18 +1,15 @@
 #pragma once
 
+typedef struct Element {
+    int number;
+    struct Element* next;
+} Element;
 
 typedef struct Liste {
     Element* first;
 } Liste;
 
-typedef struct Element {
-    int number;
-    struct Element *next;
-} Element;
-
-
-
 Liste* single_list_init();
-void single_list_insert(Liste *list, int newNumber);
-void single_list_delete(Liste *list);
+void single_list_insert(Liste* list, int newNumber);
+void single_list_delete(Liste* list);
 void single_list_display(Liste* list);
