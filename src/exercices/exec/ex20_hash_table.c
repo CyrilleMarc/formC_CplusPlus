85d48e33d6ca74c857d8d470d4566ecada02ae70
CyrilleMarc <cyrillemarc1@gmail.com>
2023-10-23 21:36:56 +0200

    ex20 hash_table

---
+++ src/exercices/exec/ex20_hash_table.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <common/common.h>
+
+int hash_table_c(char* array_to_hash) {
+    int i = 0;
+    int hashed_number = 0;
+
+    for (i = 0; array_to_hash[i] != '\0'; ++i) {
+        hashed_number += array_to_hash[i]; 
+    }
+
+    hashed_number %= 100;
+    return hashed_number;
+}
+
+int main() {
+    logger_set_current_level(LOG_INFO);
+    logger_log(LOG_DEBUG, __FUNCSIG__, "");
+
+    char name[] = "phrase à hasher";
+    int hashed_number = hash_table_c(name);
+    if(0 != hashed_number){
+        logger_log(LOG_INFO, __FUNCSIG__, "hashed number ");
+        return hashed_number;
+    }
+    else {
+        logger_log(LOG_DEBUG, __FUNCSIG__, "hashed number failure");
+        return 0;
+    }
+
+}
