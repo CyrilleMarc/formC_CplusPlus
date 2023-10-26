91ed6f5621c6627d368a6dfb6cf332fcdebdb0cc
CyrilleMarc <cyrillemarc1@gmail.com>
2023-10-26 10:40:49 +0200

    init file_folder

--- src/file/exec/test_file.c
+++ src/file/exec/test_file.c
@@ -1,10 +1,18 @@
 #include <stdlib.h>
 
 #include <common/common.h>
-#include <list/double_list.h>
+#include <file/file.h>
+
+int init_file(const char* fileName);
 
 int main(int argc, char *argv[])
 {
     logger_set_current_level(LOG_DEBUG);
     logger_log(LOG_DEBUG, __FUNCSIG__, argv[0]);
+
+    const char* fileName = "list.txt";
+    init_file(&fileName);
+
+    return 0;
+
 }
--- src/file/file.c
+++ src/file/file.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "single_list.h" 
+#include "file.h" 
 #include "common/common.h"
 
 int init_file();
